c4921dc20688b07bca9ad9ea65c1d5f618a0c3b3
42ghan <ghan@student.42seoul.kr>
2021-11-18 13:20:16 +0900

    m_fix : remove leaks check

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: ghan <ghan@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/15 14:48:45 by ghan              #+#    #+#             */
-/*   Updated: 2021/11/17 20:28:14 by ghan             ###   ########.fr       */
+/*   Updated: 2021/11/18 13:19:54 by ghan             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,5 @@ int	main(int argc, char *argv[])
 	t_spec	spec;
 
 	check_config(argc, argv, &spec);
-	system("leaks miniRT");
 	return (EXIT_SUCCESS);
 }
