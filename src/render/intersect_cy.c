44128fa74114530ba94885a449dc6b1402af4787
42ghan <ghan@student.42seoul.kr>
2021-12-10 14:14:24 +0900

    m_fix : optimize cy intersection calculation

--- src/render/intersect_cy.c
+++ src/render/intersect_cy.c
@@ -6,7 +6,7 @@
 /*   By: ghan <ghan@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/01 16:09:19 by ghan              #+#    #+#             */
-/*   Updated: 2021/12/09 19:31:17 by ghan             ###   ########.fr       */
+/*   Updated: 2021/12/10 14:13:08 by ghan             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,58 +40,52 @@ void	intersect_circle(double *ray, t_pt_info *pt_i,
 	pt_i->obj.cy = cy;
 }
 
-static double	get_cy_b(double *r, t_cy *cy, double *o)
+static double	get_cy_b(double *r, double *c, double *n, double *o)
 {
-	return (2 * (r[X] * cy->center[X] * pow(cy->o_vect[X], 2)
-			- r[X] * cy->center[X]
-			+ r[X] * cy->center[Y] * cy->o_vect[X] * cy->o_vect[Y]
-			+ r[X] * cy->center[Z] * cy->o_vect[X] * cy->o_vect[Z]
-			- r[X] * pow(cy->o_vect[X], 2) * o[X]
-			- r[X] * cy->o_vect[X] * cy->o_vect[Y] * o[Y]
-			- r[X] * cy->o_vect[X] * cy->o_vect[Z] * o[Z]
-			+ r[X] * o[X] + r[Y] * cy->center[X]
-			* cy->o_vect[X] * cy->o_vect[Y]
-			+ r[Y] * cy->center[Y]
-			* pow(cy->o_vect[Y], 2) - r[Y] * cy->center[Y]
-			+ r[Y] * cy->center[Z] * cy->o_vect[Y] * cy->o_vect[Z]
-			- r[Y] * cy->o_vect[X] * cy->o_vect[Y] * o[X]
-			- r[Y] * pow(cy->o_vect[Y], 2) * o[Y]
-			- r[Y] * cy->o_vect[Y] * cy->o_vect[Z] * o[Z] + r[Y] * o[Y]
-			+ r[Z] * cy->center[X] * cy->o_vect[X] * cy->o_vect[Z]
-			+ r[Z] * cy->center[Y] * cy->o_vect[Y] * cy->o_vect[Z]
-			+ r[Z] * cy->center[Z]
-			* pow(cy->o_vect[Z], 2) - r[Z] * cy->center[Z]
-			- r[Z] * cy->o_vect[X] * cy->o_vect[Z] * o[X]
-			- r[Z] * cy->o_vect[Y] * cy->o_vect[Z] * o[Y]
-			- r[Z] * pow(cy->o_vect[Z], 2) * o[Z] + r[Z] * o[Z]));
+	double	r_x_n_x;
+	double	r_y_n_y;
+	double	r_z_n_z;
+	double	n_x_o_x;
+	double	n_y_o_y;
+
+	r_x_n_x = r[X] * n[X];
+	r_y_n_y = r[Y] * n[Y];
+	r_z_n_z = r[Z] * n[Z];
+	n_x_o_x = n[X] * o[X];
+	n_y_o_y = n[Y] * o[Y];
+	return (2 * (r_x_n_x * c[X] * n[X] - r[X] * c[X] + r_x_n_x * c[Y] * n[Y]
+			+ r_x_n_x * c[Z] * n[Z] - r_x_n_x * n_x_o_x - r_x_n_x * n_y_o_y
+			- r_x_n_x * n[Z] * o[Z] + r[X] * o[X] + r_y_n_y * c[X] * n[X]
+			+ r_y_n_y * c[Y] * n[Y] - r[Y] * c[Y] + r_y_n_y * c[Z] * n[Z]
+			- r_y_n_y * n_x_o_x - r_y_n_y * n_y_o_y - r_y_n_y * n[Z] * o[Z]
+			+ r[Y] * o[Y] + r_z_n_z * c[X] * n[X] + r_z_n_z * c[Y] * n[Y]
+			+ r_z_n_z * c[Z] * n[Z] - r[Z] * c[Z] - r_z_n_z * n_x_o_x
+			- r_z_n_z * n_y_o_y - r_z_n_z * n[Z] * o[Z] + r[Z] * o[Z]));
 }
 
-static double	get_cy_c(t_cy *cy, double *o, double rad)
+static double	get_cy_c(double *c, double *n, double *o, double rad)
 {
-	return (pow(vect_size(cy->center), 2) + pow(vect_size(o), 2)
-		+ 2 * (cy->center[X] * pow(cy->o_vect[X], 2) * o[X]
-			- cy->center[X] * cy->center[Y] * cy->o_vect[X] * cy->o_vect[Y]
-			- cy->center[X] * cy->center[Z] * cy->o_vect[X] * cy->o_vect[Z]
-			+ cy->center[X] * cy->o_vect[X] * cy->o_vect[Y] * o[Y]
-			+ cy->center[X] * cy->o_vect[X] * cy->o_vect[Z] * o[Z]
-			- cy->center[X] * o[X]
-			- cy->center[Y] * cy->center[Z] * cy->o_vect[Y] * cy->o_vect[Z]
-			+ cy->center[Y] * cy->o_vect[X] * cy->o_vect[Y] * o[X]
-			+ cy->center[Y] * pow(cy->o_vect[Y], 2) * o[Y]
-			+ cy->center[Y] * cy->o_vect[Y] * cy->o_vect[Z] * o[Z]
-			- cy->center[Y] * o[Y]
-			+ cy->center[Z] * cy->o_vect[X] * cy->o_vect[Z] * o[X]
-			+ cy->center[Z] * cy->o_vect[Y] * cy->o_vect[Z] * o[Y]
-			+ cy->center[Z] * pow(cy->o_vect[Z], 2) * o[Z]
-			- cy->center[Z] * o[Z]
-			- cy->o_vect[X] * cy->o_vect[Y] * o[X] * o[Y]
-			- cy->o_vect[X] * cy->o_vect[Z] * o[X] * o[Z]
-			- cy->o_vect[Y] * cy->o_vect[Z] * o[Y] * o[Z])
-		- pow(cy->center[Y] * cy->o_vect[Y], 2)
-		- pow(cy->center[Z] * cy->o_vect[Z], 2)
-		- pow(cy->o_vect[X] * o[X], 2) - pow(cy->o_vect[Y] * o[Y], 2)
-		- pow(cy->o_vect[Z] * o[Z], 2) - pow(rad, 2)
-		- pow(cy->center[X] * cy->o_vect[X], 2));
+	double	c_x_n_x;
+	double	c_y_n_y;
+	double	c_z_n_z;
+	double	n_x_o_x;
+	double	n_y_o_y;
+
+	c_x_n_x = c[X] * n[X];
+	c_y_n_y = c[Y] * n[Y];
+	c_z_n_z = c[Z] * n[Z];
+	n_x_o_x = n[X] * o[X];
+	n_y_o_y = n[Y] * o[Y];
+	return (pow(vect_size(c), 2) + pow(vect_size(o), 2)
+		+ 2 * (c_x_n_x * n_x_o_x - c_x_n_x * c_y_n_y - c_x_n_x * c[Z] * n[Z]
+			+ c_x_n_x * n_y_o_y + c_x_n_x * n[Z] * o[Z] - c[X] * o[X]
+			- c_y_n_y * c[Z] * n[Z] + c_y_n_y * n_x_o_x + c_y_n_y * n_y_o_y
+			+ c_y_n_y * n[Z] * o[Z] - c[Y] * o[Y] + c_z_n_z * n_x_o_x
+			+ c_z_n_z * n_y_o_y + c_z_n_z * n[Z] * o[Z] - c[Z] * o[Z]
+			- n[X] * n[Y] * o[X] * o[Y] - n[X] * n[Z] * o[X] * o[Z]
+			- n[Y] * n[Z] * o[Y] * o[Z])
+		- pow(c_y_n_y, 2) - pow(c_z_n_z, 2) - pow(n_x_o_x, 2) - pow(n_y_o_y, 2)
+		- pow(n[Z] * o[Z], 2) - pow(rad, 2) - pow(c_x_n_x, 2));
 }
 
 static double	get_pt_on_cy(double *r, t_cy *cy, double *o, double rad)
@@ -105,8 +99,8 @@ static double	get_pt_on_cy(double *r, t_cy *cy, double *o, double rad)
 			+ r[Y] * r[Z] * cy->o_vect[Y] * cy->o_vect[Z])
 		- pow(r[X] * cy->o_vect[X], 2) - pow(r[Y] * cy->o_vect[Y], 2)
 		- pow(r[Z] * cy->o_vect[Z], 2);
-	b = get_cy_b(r, cy, o);
-	c = get_cy_c(cy, o, rad);
+	b = get_cy_b(r, cy->center, cy->o_vect, o);
+	c = get_cy_c(cy->center, cy->o_vect, o, rad);
 	if (pow(b, 2) - 4 * a * c < 0)
 		return (0);
 	return ((-1 * b - sqrt(pow(b, 2) - 4 * a * c)) / (2 * a));
